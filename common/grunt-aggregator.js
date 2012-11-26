c39b5deacd10fb62c5f9d3e32027b836a43370f2
Omry Nachman <omri@wix.com>
2012-11-26 17:25:51 +0200

    futal attempts to make npm global flag to work

---
+++ common/grunt-aggregator.js
@@ -0,0 +1,8 @@
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-jasmine-node');
+grunt.loadNpmTasks('grunt-clean');
+
+// Actually load this plugin's task(s).
+grunt.loadTasks('tasks');
+
+module.exports = {};
